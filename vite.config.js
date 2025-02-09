743cbbd59c72496c3e5ffffb593715288eddbeec
Shubham <shubh.shubhamkrsingh@gmail.com>
2025-02-09 22:26:53 +0530

    not

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/CocaCola-Reimagined/",
-  build: {
-    outDir: "dist",
-    assetsInlineLimit: 0,
-  }
+
 })
