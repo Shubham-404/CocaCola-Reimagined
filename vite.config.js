fee7982e24d8bd80362908e1236a1f0c22e8dd70
Shubham <shubh.shubhamkrsingh@gmail.com>
2025-02-09 22:21:38 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/CocaCola-Reimagined/"
+  base: "/CocaCola-Reimagined/",
+  build: {
+    outDir: "dist",
+    assetsInlineLimit: 0,
+  }
 })
